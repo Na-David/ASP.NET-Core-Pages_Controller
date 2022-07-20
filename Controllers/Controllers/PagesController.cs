eb50f13ba10e0ff2036fc999c3f4f36dfa9486e0
DESKTOP-PB7NIIJ\hna20 <hna206@naver.com>
2022-07-21 00:52:41 +1200

    Add project files.

---
+++ Controllers/Controllers/PagesController.cs
@@ -0,0 +1,26 @@
+﻿using Controllers.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Controllers.Controllers
+{
+    public class PagesController : Controller
+    {
+        //private readonly ILogger<HomeController> _logger;
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult NotFound()
+        {
+            return View();
+        }
+    }
+}
