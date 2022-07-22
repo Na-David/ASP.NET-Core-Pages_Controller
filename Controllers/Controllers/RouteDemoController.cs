c84354c77d65651235e250c2dd5fb8ff3ecf24e3
DESKTOP-PB7NIIJ\hna20 <hna206@naver.com>
2022-07-23 02:27:00 +1200

    Route Attribute Demo(Route()) // Attribute Routing

---
+++ Controllers/Controllers/RouteDemoController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Controllers.Controllers
+{
+    public class RouteDemoController
+    {
+        [Route("RouteDemoTest")]
+        public string Index()
+        {
+            return "Hello World!";
+        }
+
+        //public IActionResult Index()
+        //{
+            //WHen we add a new controller and has inherit Controller class, then Controller name is RouteDemo.
+            //It means when we open a new windows, www.localhost5000.com/RouteDemo will be work. Index is normal
+            //If you don't want inherit Controller class, then we need to use Route attribute like this
+        //    return Content("Hello World!");
+       // }
+    }
+}
